823d29a3d96b7f8e58e0c0bb233aed8652ab410e
dan410 <dan410@gmail.com>
2014-02-27 13:59:34 -0600

    added some comments to main.R. After this commit I intend to make some major revisions

--- main.R
+++ main.R
@@ -7,18 +7,24 @@ library(ggplot2)
 source('run-sims.R')
 source('L2norm.R')
 
-# set the working directory
-setwd(dir = "/Users/dan410/Google Drive/Research/Projects/SimStudy_eigenfunction_estimation")
-# set the parameters
+# NOTE: set the current working directory to this project using setwd(dir = "")
+
+# Specify the parameter file to use. These files are in the params folder.
 paramsfile = "params-dep-1.rds"
 params <- readRDS(file = paste('params/', paramsfile, sep=""))
 
+# Run simulations using the specified parameters
 results <- run_sims(n.iter = 30, m = params$m, levels.m = params$levels.m, type = params$type, basis.pars = params$basis.pars, sigma0 = params$sigma0, rand.obs = params$rand.obs, n.dim = params$n.dim, n.marginal.knots = params$n.marginal.knots, cov.model = params$cov.model, cov.pars = params$cov.pars, locs = params$locs)$results
 
+# Save results to the results folder
 saveRDS(results, file = paste('results/results-', paramsfile, sep=""))
 
+# Read in results object
 results <- readRDS(file = paste('results/results-', paramsfile, sep=""))
 
+#########################
+# To measure performance, compute L2 norm of difference between estimated and true EFs
+#########################
 n.iter = 30
 ef1 <- numeric(n.iter)
 ef2 <- numeric(n.iter)
@@ -26,7 +32,7 @@ for( i in 1:n.iter){
     ef1[i] <- L2norm1(results[[i]][[2]]$eig.est)
     ef2[i] <- L2norm2(results[[i]][[2]]$eig.est)
 }
-
+# Summary statistics
 mean(ef1)
 sd(ef1)
 mean(ef2)
