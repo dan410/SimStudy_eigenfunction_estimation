7528ea22f8f3a8a315e04224c660f17539dce50f
dan410 <dan410@gmail.com>
2014-03-03 16:01:41 -0600

    - Create L2-summary.R to compute L2 norms and calculate summary statistics.
    - Update gitignore to ignore cache folder

---
+++ R/L2-summary.R
@@ -0,0 +1,22 @@
+
+#' compute L2 norm of difference between estimated and true EFs as well as summary stats
+#' 
+#' compute L2 norm of difference between estimated and true EFs as well as summary stats
+#' 
+#' @param fpc list with each element a list of the first two FPCs
+#' @param round.digits integer indicating the number of decimal places to be used. Negative values are allowed (see ?base::round for details)
+#' @return data frame containing summary statistics 
+L2_summary <- function(fpc, round.digits = 4){
+   FPC.L2 <- ldply(fpc, .fun = fpca_L2norm)
+   stopifnot(is.data.frame(FPC.L2))
+   
+   # use ddply "summarise" option to compute summary statistics
+   summary <- ddply(FPC.L2, .(m, sigma0), summarise,
+         median.PC1 = median(L2fpc1),
+         mean.PC1 = mean(L2fpc1),
+         se.PC1 = sd(L2fpc1)/sqrt(length(L2fpc1)),
+         median.PC2 = median(L2fpc2),
+         mean.PC2 = mean(L2fpc2),
+         se.PC2 = sd(L2fpc2)/sqrt(length(L2fpc2)))
+   return(round(summary, digits = round.digits))
+}
