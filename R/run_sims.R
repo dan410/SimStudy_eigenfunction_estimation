5a1fc97cff529553ca2921d935c26438dc45c74a
dan410 <dan410@gmail.com>
2014-03-14 00:03:52 -0500

    Run sims

--- R/run_sims.R
+++ R/run_sims.R
@@ -15,30 +15,32 @@ params <- readRDS(file = paste('analysis/params/', paramsfile, sep=""))
  
  #### Estimate the principal component functions for each data set and sore them in a list
  ### using smoothing spline method ###
- n.marginal.knots <- 7
+ n.marginal.knots <- NULL
+ eps <- 0.001
+ marginal.knots <- seq(0+eps,1-eps, length=7)
 
 FPC <- list() #initialize empty list
 for(i in 1:n.data.sets){
-  FPC[[i]] <- fpca2_ss(SIMDAT[[i]], n.marginal.knots=n.marginal.knots)
+  FPC[[i]] <- fpca2_ss(SIMDAT[[i]], n.marginal.knots=n.marginal.knots, marginal.knots=marginal.knots)
 }
 
 saveRDS(FPC, file = paste("analysis/results/fpca-ss-", paramsfile, sep=""))
 
  
-################### FDA Package using pca.fd() ############
-
-### Here we use B-spline basis with breaks that depend on the number of knots used in the smoothing spline estimator
-rangeval <- c(0,1)
-breaks <- min(rangeval) + (max(rangeval) - min(rangeval)) * (1:n.marginal.knots)/(n.marginal.knots + 1)
-breaks <- c(rangeval[1], breaks, rangeval[2]) # first and last break point must be min/max range value
-
-### 
-FPC <- list()
-for(i in 1:n.data.sets){
- FPC[[i]] <- fpca2_fda(sets=SIMDAT[[i]], rangeval = rangeval, norder = 4, nbasis=5)
-}
-
-saveRDS(FPC, file = paste("analysis/results/fpca-fda-", paramsfile, sep=""))
+# ################### FDA Package using pca.fd() ############
+# 
+# ### Here we use B-spline basis with breaks that depend on the number of knots used in the smoothing spline estimator
+# rangeval <- c(0,1)
+# #breaks <- min(rangeval) + (max(rangeval) - min(rangeval)) * (1:n.marginal.knots)/(n.marginal.knots + 1)
+# #breaks <- c(rangeval[1], breaks, rangeval[2]) # first and last break point must be min/max range value
+# 
+# ### 
+# FPC <- list()
+# for(i in 1:n.data.sets){
+#  FPC[[i]] <- fpca2_fda(sets=SIMDAT[[i]], rangeval = rangeval, norder = 4, nbasis=5)
+# }
+# 
+# saveRDS(FPC, file = paste("analysis/results/fpca-fda-", paramsfile, sep=""))
 
 return(0)
 }
--- analysis/main.R
+++ analysis/main.R
@@ -1,6 +1,6 @@
 # load required packages and required functions
 source("libraries/libs.R")
 
-char2seed("Fortin")
-run_sims(n.data.sets = 2, paramsfile = "params-ind-3.rds")
+#char2seed("Fortin")
+run_sims(n.data.sets = 50, paramsfile = "params-ind-3.rds")
 
