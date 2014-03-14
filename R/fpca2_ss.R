f3dad149f69364b6b76873e97fa92d94489fb66d
dan410 <dan410@gmail.com>
2014-03-13 23:19:35 -0500

    Update functions involved with calling estimate_cov_function() to pass marginal.knots argument. This is so that user-specified knot location s can be give as an argument, instead of just the number of knot locations.

--- R/fpca2_ss.R
+++ R/fpca2_ss.R
@@ -4,9 +4,9 @@
 #' @param sets list of data sets as output from \code{sim_data()}
 #' @param n.marginal.knots number of knot locations to use on the marginal domain. The number of knot locations actually used in estimation will be n^2 on the product domain. Default is 5.
 #' @return list with each element a list containing the first two estimate FPCs
-fpca2_ss <- function(sets, n.marginal.knots=5){
+fpca2_ss <- function(sets, n.marginal.knots=NULL, marginal.knots=NULL){
   df <- ldply(sets, function(x){x}) #converting list to data frame
   
-  FPC <- dlply(df, .variables=c("m","sigma0"), .fun = fpca_ss, n.marginal.knots)
+  FPC <- dlply(df, .variables=c("m","sigma0"), .fun = fpca_ss, n.marginal.knots=n.marginal.knots, marginal.knots=marginal.knots)
   return(FPC)
 }
--- R/fpca_ss.R
+++ R/fpca_ss.R
@@ -5,8 +5,8 @@
 #' @param dat functional data set that can be passed to \code{ssfcov2::estimate_cov_function()}. See documentation for details.
 #' @param n.marginal.knots number of knot locations to use on the marginal domain. The number of knot locations actually used in estimation will be n^2 on the product domain.
 #' @return list containing first two principal component functions
-fpca_ss <- function(dat, n.marginal.knots){ 
-  cov.est <- estimate_cov_function(dat, n.marginal.knots = n.marginal.knots)
+fpca_ss <- function(dat, n.marginal.knots=NULL, marginal.knots=NULL){ 
+  cov.est <- estimate_cov_function(dat, n.marginal.knots = n.marginal.knots, marginal.knots=marginal.knots)
   eig.est <- estimate_eigenfunctions(cov.est)
   fpc1 <- extract_pcf(nharm = 1, method = 'ss', eig.est)
   fpc2 <- extract_pcf(nharm = 2, method = 'ss', eig.est)
