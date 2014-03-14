b462eaf1f54152aa832dd87093f82ce7ef7058df
dan410 <dan410@gmail.com>
2014-03-13 20:03:41 -0500

    Increase number of data sets to run on linux server

--- analysis/main.R
+++ analysis/main.R
@@ -1,5 +1,5 @@
 # load required packages and required functions
 source("libraries/libs.R")
 
-run_sims(n.data.sets = 5, paramsfile = "params-ind-3.rds")
+run_sims(n.data.sets = 50, paramsfile = "params-ind-3.rds")
 
