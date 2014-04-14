f105035f4b7b5b64590dd08c2745e1e5dcf231fe
dan410 <dan410@gmail.com>
2014-04-14 12:02:22 -0500

    Update code for creating gaps of missing data in the simulated data. Modified the data set with 20 observations by creating 5-obs gaps.

--- data/create-data.R
+++ data/create-data.R
@@ -1,5 +1,5 @@
 
-paramsfile = "params-ind-50.rds"
+paramsfile = "params-ind-20.rds"
 params <- readRDS(file = paste('analysis/params/', paramsfile, sep=""))
 
 ### How many data sets do you want to use?
@@ -13,3 +13,11 @@ for(i in 1:n.data.sets){
 }
 
 saveRDS(dat, file = paste("data/data-", paramsfile, sep=""))
+
+### Use this code to create gaps in the data in order to investigate the effect of chunks of missing data
+dat <- readRDS("data/data-params-ind-20.rds")
+gapdat <- NULL
+for( i in 1:length(dat)){
+  gapdat[[i]] <- ddply(dat[[i]], .(ID), create_gap, start=15, length=5)
+}
+
