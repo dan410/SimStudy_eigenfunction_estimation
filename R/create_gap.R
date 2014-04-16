61bf69fb673d9ce0dfce205c307a2a68470fda27
dan410 <dan410@gmail.com>
2014-04-16 14:11:37 -0500

    Update create_gap.R to allow randomly spaced missing gaps in the data

--- R/create_gap.R
+++ R/create_gap.R
@@ -1,10 +1,16 @@
 
 create_gap <- function(dat, start, length, beginning=TRUE){
-  if(!beginning){
-    sub <- order(dat$Time, decreasing=TRUE)[start:(start+length-1)]
-  }else{
+  if(start=="random"){
+    start = sample(1:(length(dat$Time) - length+1), size=1)
     sub <- order(dat$Time)[start:(start+length-1)]
+  }else{
+    if(!beginning){
+      sub <- order(dat$Time, decreasing=TRUE)[start:(start+length-1)]
+    }else{
+      sub <- order(dat$Time)[start:(start+length-1)]
+    } 
   }
+  
   return(dat[-sub,])
 }
 
