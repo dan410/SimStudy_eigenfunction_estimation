5e24462b135a64e0b42be3d11bea97cb082e1998
dan410 <dan410@gmail.com>
2014-04-16 14:10:47 -0500

    Create a new 'gap' data set

--- data/create-data.R
+++ data/create-data.R
@@ -17,7 +17,27 @@ saveRDS(dat, file = paste("data/data-", paramsfile, sep=""))
 ### Use this code to create gaps in the data in order to investigate the effect of chunks of missing data
 dat <- readRDS("data/data-params-ind-20.rds")
 gapdat <- NULL
-for( i in 1:length(dat)){
-  gapdat[[i]] <- ddply(dat[[i]], .(ID), create_gap, start=15, length=5)
+for( i in 1:50){
+  gapdat[[i]] <- ddply(dat[[i]], .(ID), create_gap, start=16, length=5, beginning=TRUE)
+}
+for(i in 51:100){
+  gapdat[[i]] <- ddply(dat[[i]], .(ID), create_gap, start=16, length=5, beginning=FALSE)
 }
 
+gp <- ggplot(gapdat[[51]], aes(x=Time, y=X, group=ID))
+gp + geom_line()
+
+saveRDS(gapdat, file = "data/gapdata-params-ind-20.rds")
+
+### Create gaps at random places
+dat <- readRDS("data/data-params-ind-20.rds")
+gapdat <- NULL
+for( i in 1:100){
+  gapdat[[i]] <- ddply(dat[[i]], .(ID), create_gap, start="random", length=5)
+}
+
+
+gp <- ggplot(gapdat[[1]], aes(x=Time, y=X, group=ID))
+gp + geom_line()
+
+saveRDS(gapdat, file = "data/gapdata2-params-ind-20.rds")
