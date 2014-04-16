0f581fb742a97cce81f0bee003c3c156cec517eb
dan410 <dan410@gmail.com>
2014-04-16 14:12:01 -0500

    Update plotting code

--- plot-pcfs.R
+++ plot-pcfs.R
@@ -1,21 +1,45 @@
 
 ### L2-norm summary statistic
 library(xtable)
-#junk <- readRDS("analysis/results/fpca-fda-params-ind-3.rds")
-fit.ss <- readRDS("analysis/results/fpca-ss-params-ind-20.rds")
-fit.fda <- readRDS("analysis/results/fpca-fda-params-ind-20.rds")
-fit.ss <- readRDS("cache/fpca-ss-params-ind-3.rds")
-( res.ss <- L2_summary(fit.ss))
-fit.fda <- readRDS("cache/fpca-fda-params-ind-3.rds") 
-(res.fda <- L2_summary(fit.fda))
+fit.ss20 <- readRDS("analysis/results/fpca-ss-params-ind-20.rds")
+fit.fda20 <- readRDS("analysis/results/fpca-fda-params-ind-20.rds")
 
-xtable(res.ss,digits=3)
-xtable(res.fda, digits=3)
+fit.ss5 <- readRDS("cache/fpca-ss-params-ind-5.rds")
+fit.ss10 <- readRDS("cache/fpca-ss-params-ind-10.rds")
+fit.ss20 <- readRDS("cache/fpca-ss-params-ind-20.rds")
+res.ss5 <- L2_summary(fit.ss5)
+res.ss10 <- L2_summary(fit.ss10)
+res.ss20 <- L2_summary(fit.ss20)
+
+fit.fda5 <- readRDS("cache/fpca-fda-params-ind-5.rds")
+fit.fda10 <- readRDS("cache/fpca-fda-params-ind-10.rds")
+fit.fda20 <- readRDS("cache/fpca-fda-params-ind-20.rds")
+res.fda5 <- L2_summary(fit.fda5)
+res.fda10 <- L2_summary(fit.fda10)
+res.fda20 <- L2_summary(fit.fda20)
+
+r1 <- cbind(res.ss5[,1:3], res.fda5[,1:3])
+r2 <- cbind(res.ss10[,1:3], res.fda10[,1:3])
+r3 <- cbind(res.ss20[,1:3], res.fda20[,1:3])
+r4 <- cbind(res.ss5[,4:6], res.fda5[,4:6])
+r5 <- cbind(res.ss10[,4:6], res.fda10[,4:6])
+r6 <- cbind(res.ss20[,4:6], res.fda20[,4:6])
+
+fpc1 <- rbind(r1, r2, r3)
+fpc1 <- fpc1[,-c(1,4)]
+xtable(fpc1, digits = 4)
+
+fpc2 <- rbind(r4, r5, r6)
+fpc2 <- fpc2[, -c(1,4)]
+xtable(fpc2, digits = 4)
 
 ### plot principal component functions with pointwise confidence bands
-FPC <- readRDS("cache/fpca-ss-params-ind-20.rds")
-
+FPC <- readRDS("analysis/results/fpca-ss-params-ind-20.rds")
+FPC <- readRDS("cache/fpca-fda-params-ind-20.rds")
+file = "fpca-ss-params-ind-20"
+FPC <- readRDS(paste("cache/",file, ".rds" ,sep=""))
 # PC1
+n <- length(FPC)
 eval.xs <- 1:1000/1001
 eval.mat <- matrix(0, nrow= n+1, ncol = length(eval.xs))
 eval.mat[1,] <- fpc1_true(eval.xs)
@@ -28,12 +52,14 @@ limits <- apply(eval.mat[-1,], 2, quantile, probs = c(0.025, .975))
 avg <- apply(eval.mat[-1,], 2, mean)
 df <- data.frame(x = eval.xs, avg = avg, lower = limits[1,], upper = limits[2,], truth = eval.mat[1,])
 ggp <- ggplot(df, aes(x = x, y = avg))
-ggp <- ggp + geom_line() + geom_line(aes(y=truth), color="red")
+ggp <- ggp + geom_line() + geom_line(aes(y=truth), linetype="dashed")
 ggp <- ggp + geom_ribbon(aes(ymin=lower, ymax = upper), alpha= 0.3)
-ggp <- ggp + xlab('') + ylab('') + theme_bw()
+ggp <- ggp + xlab('') + ylab('1st FPC') + theme_bw()
+ggp <- ggp + theme(axis.title.y = element_text(size = rel(1.2), angle = 90))
 ggp 
-  
+ggsave(paste("figure/fpc1-", file, ".pdf", sep=""))
 # PC2
+n <- length(FPC)
 eval.xs <- 1:1000/1001
 eval.mat <- matrix(0, nrow= n+1, ncol = length(eval.xs))
 eval.mat[1,] <- fpc2_true(eval.xs)
@@ -46,11 +72,13 @@ limits <- apply(eval.mat[-1,], 2, quantile, probs = c(0.025, .975))
 avg <- apply(eval.mat[-1,], 2, mean)
 df <- data.frame(x = eval.xs, avg = avg, lower = limits[1,], upper = limits[2,], truth = eval.mat[1,])
 ggp <- ggplot(df, aes(x = x, y = avg))
-ggp <- ggp + geom_line() + geom_line(aes(y=truth), color="red")
+ggp <- ggp + geom_line() + geom_line(aes(y=truth), linetype="dashed")
 ggp <- ggp + geom_ribbon(aes(ymin=lower, ymax = upper), alpha= 0.3)
-ggp <- ggp + xlab('') + ylab('') + theme_bw()
+ggp <- ggp + xlab('') + ylab('2nd FPC') + theme_bw()
+ggp <- ggp + theme(axis.title.y = element_text(size = rel(1.2), angle = 90))
 ggp 
 
+ggsave(paste("figure/fpc2-", file, ".pdf", sep=""))
 
 
 
