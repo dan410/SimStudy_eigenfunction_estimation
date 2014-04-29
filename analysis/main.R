d9a18096ee4d04e9e66e9ebcfccd6582a70fb5bd
dan410 <dan410@gmail.com>
2014-04-29 09:43:11 -0500

    Update main.R to fit the 'gap' data on linux10

--- analysis/main.R
+++ analysis/main.R
@@ -2,10 +2,10 @@
 source("libraries/libs.R")
 
 
-paramsfile <- "params-ind-20.rds" 
-dat <- readRDS(paste("data/data-", paramsfile, sep=''))
-run_calc(dat = dat, paramsfile = paramsfile, method = "ssfcov2")
-run_calc(dat = dat, paramsfile = paramsfile, method = "fda")
+# paramsfile <- "params-ind-20.rds" 
+# dat <- readRDS(paste("data/data-", paramsfile, sep=''))
+# run_calc(dat = dat, paramsfile = paramsfile, method = "ssfcov2")
+# run_calc(dat = dat, paramsfile = paramsfile, method = "fda")
 
 ## for "params-ind-50.rds" I used lambda = 10^-3.75
 ## for "params-ind-20.rds" I used lambda = 10^-3.75
@@ -29,19 +29,19 @@ run_calc(dat = dat, paramsfile = paramsfile, method = "ssfcov2")
 run_calc(dat = dat, paramsfile = paramsfile, method = "fda")
 
 
-### fit the beta data ####
-paramsfile <- "params-ind-5.rds" 
-dat <- readRDS(paste("data/betadata-", paramsfile, sep=''))
-run_calc(dat = dat, paramsfile = paramsfile, method = "ssfcov2")
-run_calc(dat = dat, paramsfile = paramsfile, method = "fda")
-
-paramsfile <- "params-ind-20.rds" 
-dat <- readRDS(paste("data/betadata-", paramsfile, sep=''))
-run_calc(dat = dat, paramsfile = paramsfile, method = "ssfcov2")
-run_calc(dat = dat, paramsfile = paramsfile, method = "fda")
-
-paramsfile <- "params-ind-10.rds" 
-dat <- readRDS(paste("data/betadata-", paramsfile, sep=''))
-run_calc(dat = dat, paramsfile = paramsfile, method = "ssfcov2")
-run_calc(dat = dat, paramsfile = paramsfile, method = "fda")
+# ### fit the beta data ####
+# paramsfile <- "params-ind-5.rds" 
+# dat <- readRDS(paste("data/betadata-", paramsfile, sep=''))
+# run_calc(dat = dat, paramsfile = paramsfile, method = "ssfcov2")
+# run_calc(dat = dat, paramsfile = paramsfile, method = "fda")
+# 
+# paramsfile <- "params-ind-20.rds" 
+# dat <- readRDS(paste("data/betadata-", paramsfile, sep=''))
+# run_calc(dat = dat, paramsfile = paramsfile, method = "ssfcov2")
+# run_calc(dat = dat, paramsfile = paramsfile, method = "fda")
+# 
+# paramsfile <- "params-ind-10.rds" 
+# dat <- readRDS(paste("data/betadata-", paramsfile, sep=''))
+# run_calc(dat = dat, paramsfile = paramsfile, method = "ssfcov2")
+# run_calc(dat = dat, paramsfile = paramsfile, method = "fda")
 
